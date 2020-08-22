335932d255facd318f7acae3a2bc126a6d8491d1
kh0j1 <69531634+kh0j1@users.noreply.github.com>
2020-08-22 18:32:04 +0500

    Updates

---
+++ precache-manifest.c672b932988b781cc5b48d020411c12a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a53cb2ef99288dde11b15ad95da3b63",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "2df5277a5466f6439662",
+    "url": "/todolist/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "2df5277a5466f6439662",
+    "url": "/todolist/static/js/2.05031881.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/todolist/static/js/2.05031881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae238f6caa68b4cc7314",
+    "url": "/todolist/static/js/main.f2739d0f.chunk.js"
+  },
+  {
+    "revision": "7e7339577a8e879559a7",
+    "url": "/todolist/static/js/runtime-main.486b43ec.js"
+  }
+]);
